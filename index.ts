9df340002b56e19362d326aef8f9978fb4ed5b85
Tauki Tahmid <ttauki@gmail.com>
2023-10-29 14:02:36 +0600

    warn if no servicesToClose is provided

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {defaultLogger, ILogger} from './logger';
+import { defaultLogger, ILogger } from './logger';
 
 // ServicesToClose is an interface that defines the shape of the services that will be closed
 export interface ServicesToClose { close(): Promise<void>; }
@@ -11,19 +11,40 @@ export class ShutdownManager {
   private logger: ILogger;
   private readonly servicesToClose: ServicesToClose[];
 
-  constructor(logger?: ILogger, ...servicesToClose: ServicesToClose[]);
+  constructor();
+  constructor(logger: ILogger | null);
   constructor(...servicesToClose: ServicesToClose[]);
+  constructor(logger: ILogger | null, ...servicesToClose: ServicesToClose[]);
   constructor(...args: any[]) {
-    const lastArg = args[args.length - 1];
-    if (typeof lastArg === 'object' && 'info' in lastArg && 'debug' in lastArg && 'error' in lastArg) {
-      this.logger = lastArg;
-      this.servicesToClose = args.slice(0, -1) as ServicesToClose[];
+    const [firstArg, ...restArgs] = args;
+
+    if (this.isLogger(firstArg)) {
+      this.logger = firstArg;
+      this.servicesToClose = restArgs as ServicesToClose[];
+    } else if (firstArg === null) {
+      this.logger = noOpLogger;
+      this.servicesToClose = restArgs as ServicesToClose[];
     } else {
       this.logger = defaultLogger;
-      this.servicesToClose = args as ServicesToClose[];
+      this.servicesToClose = args.length > 0 ? [firstArg, ...restArgs] as ServicesToClose[] : [];
     }
+
+    if (this.servicesToClose.length === 0) {
+      this.logger.warn('ShutdownManager instantiated without any services to close.');
+    }
+
     this.logger.debug('Waiting for OS signals...');
     this.init();
+  }
+
+  private isLogger(arg: unknown): arg is ILogger {
+    return (
+        typeof arg === 'object' &&
+        arg !== null &&
+        'info' in arg &&
+        'debug' in arg &&
+        'error' in arg
+    );
   }
 
   private init(): void {
@@ -52,3 +73,10 @@ export class ShutdownManager {
     }
   }
 }
+
+export const noOpLogger: ILogger = {
+  info: () => {},
+  debug: () => {},
+  error: () => {},
+  warn: () => {},
+};
--- shutdownmanager.test.ts
+++ shutdownmanager.test.ts
@@ -1,37 +1,87 @@
-import { ShutdownManager, ServicesToClose } from './index';
+import { ShutdownManager, ServicesToClose, noOpLogger } from './index';
+import { defaultLogger } from "./logger";
 
 describe('ShutdownManager', () => {
-    it('should call the callback on SIGTERM', (done) => {
-        const mockCallback = jest.fn(() => {
-            expect(mockCallback).toHaveBeenCalled();
-            done();
-        });
+    let mockCloseFunction: jest.Mock;
+    let servicesToClose: ServicesToClose[];
 
-        const servicesToClose: ServicesToClose[] = [
-            {
-                close: async (): Promise<void> => {
-                    mockCallback();
-                }
-            }
-        ];
-        new ShutdownManager(...servicesToClose);
-        process.emit('SIGTERM');
+    beforeEach(() => {
+        mockCloseFunction = jest.fn(() => Promise.resolve());
+        servicesToClose = [{ close: mockCloseFunction }];
     });
 
-    it('should call the callback on SIGINT', (done) => {
-        const mockCallback = jest.fn(() => {
-            expect(mockCallback).toHaveBeenCalled();
-            done();
-        });
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
 
-        const servicesToClose: ServicesToClose[] = [
-            {
-                close: async (): Promise<void> => {
-                    mockCallback();
-                }
-            }
-        ];
+    it('should call the callback on SIGTERM', async () => {
+        new ShutdownManager(...servicesToClose);
+        process.emit('SIGTERM');
+        await new Promise(setImmediate);
+        expect(mockCloseFunction).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call the callback on SIGINT', async () => {
         new ShutdownManager(...servicesToClose);
         process.emit('SIGINT');
+        await new Promise(setImmediate);
+        expect(mockCloseFunction).toHaveBeenCalledTimes(1);
+    });
+
+    it('should use noOp logger if null is passed as logger', async () => {
+        const infoSpy = jest.spyOn(noOpLogger, 'info');
+        const debugSpy = jest.spyOn(noOpLogger, 'debug');
+        const errorSpy = jest.spyOn(noOpLogger, 'error');
+
+        new ShutdownManager(null, ...servicesToClose);
+        process.emit('SIGINT');
+        await new Promise(setImmediate);
+
+        expect(mockCloseFunction).toHaveBeenCalledTimes(1);
+        expect(infoSpy).toHaveBeenCalled();
+        expect(debugSpy).toHaveBeenCalled();
+        expect(errorSpy).not.toHaveBeenCalled();
+    });
+
+    it('should use default logger if no logger is passed', async () => {
+        const infoSpy = jest.spyOn(defaultLogger, 'info');
+        const debugSpy = jest.spyOn(defaultLogger, 'debug');
+        const errorSpy = jest.spyOn(defaultLogger, 'error');
+
+        new ShutdownManager(...servicesToClose);
+        process.emit('SIGINT');
+        await new Promise(setImmediate);
+
+        expect(mockCloseFunction).toHaveBeenCalledTimes(1);
+        expect(infoSpy).toHaveBeenCalled();
+        expect(debugSpy).toHaveBeenCalled();
+        expect(errorSpy).not.toHaveBeenCalled();
+    });
+
+    it('should use the passed logger', async () => {
+        const mockLogger = {
+            info: jest.fn(),
+            debug: jest.fn(),
+            error: jest.fn(),
+            warn: jest.fn(),
+        };
+
+        new ShutdownManager(mockLogger, ...servicesToClose);
+        process.emit('SIGINT');
+        await new Promise(setImmediate);
+
+        expect(mockCloseFunction).toHaveBeenCalledTimes(1);
+        expect(mockLogger.info).toHaveBeenCalled();
+        expect(mockLogger.debug).toHaveBeenCalled();
+        expect(mockLogger.error).not.toHaveBeenCalled();
+    });
+
+    it('should log a warning if no services are passed', () => {
+        const warnSpy = jest.spyOn(defaultLogger, 'warn');
+        const debugSpy = jest.spyOn(defaultLogger, 'debug');
+
+        new ShutdownManager();
+        expect(warnSpy).toHaveBeenCalled();
+        expect(debugSpy).toHaveBeenCalled();
     });
 });
